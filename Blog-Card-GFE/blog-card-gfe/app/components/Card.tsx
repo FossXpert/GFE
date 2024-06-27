491239e139bb59371f1abb1c5116f53d7b3f0bc3
Rahul Ray <FossXpert@outlook.com>
2024-06-27 08:39:34 +0000

    blog card gfe

---
+++ Blog-Card-GFE/blog-card-gfe/app/components/Card.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from "../page.module.css";
+
+
+type Props = {
+    
+}
+
+const Card = (props: Props) => {
+  return (
+    <main className={styles.main}>
+      <div>
+        
+      </div>
+    </main>
+    
+  )
+}
+
+export default Card

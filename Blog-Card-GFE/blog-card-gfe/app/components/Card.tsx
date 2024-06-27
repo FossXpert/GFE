4b36d7398437401414fac980c1b74fb834a37ca9
Rahul Ray <FossXpert@outlook.com>
2024-06-27 12:12:08 +0000

    rnn

--- Blog-Card-GFE/blog-card-gfe/app/components/Card.tsx
+++ Blog-Card-GFE/blog-card-gfe/app/components/Card.tsx
@@ -1,19 +1,33 @@
 import React from 'react'
 import styles from "../page.module.css";
+import Image from 'next/image';
+import image from '../assets/spacejoy.jpg'
 
 
 type Props = {
-    
 }
 
 const Card = (props: Props) => {
   return (
-    <main className={styles.main}>
-      <div>
-        
+    <>
+    <div className={styles.container}>
+      <div className={styles.box}>
+        <div className={styles.image}>
+          <Image src={image} alt='Space Joy' width={290} className={styles.imagea}/>
+        </div>
+        <div className={styles.below}>
+          <div className={styles.exterior}>
+          <p className={styles.interior}>Interior</p>
+          </div>
+          <h4 className={styles.h4}>Top 5 Living Room Inspirations</h4>
+          <p className={styles.curated}>Curated vibrants colors for your living, make it pop & calm in the same time</p>
+          <div className={styles.readMore}> 
+            Read More -{`>`}
+          </div>
+        </div>
       </div>
-    </main>
-    
+    </div>
+    </>
   )
 }
 

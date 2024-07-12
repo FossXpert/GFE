d538538e5d56c9154bfd0159806d47e5e7baec91
rahulray8518 <rahulray8518@gmail.com>
2024-07-12 06:34:41 +0000

    fff

---
+++ Project/navbar-ecom-gfe/app/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react"
+import './navbar.css';
+
+type Props = {
+    open : boolean
+}
+
+const Navbar = ()=>{
+    return(
+        <>
+        
+        </>
+    )
+
+}
+
+export default Navbar;
